f70459fc27f2806be4099efe5a911810afb9fd2b
Karolina <karolina.saegner@uksh.de>
2025-01-21 16:03:51 +0100

    updated the identification of relevant files (including RUN ('on' and 'off))

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import sys
 import pandas as pd
 import src
+import os
 
 
 if sys.platform == "linux":
@@ -26,6 +27,9 @@ TASK_NAMES = [
 TRACKING_SYSTEMS = [
     "omc"
 ] # add "imu" if needed
+RUN = [
+    'on'
+] # add 'off' if needed 
 
 
 def main() -> None:
@@ -41,31 +45,58 @@ def main() -> None:
     # for row_idx, row in matched_control_sub_ids.iterrows():
         # sub_id = f"pp{row['id']:>03d}"
         
-        # file name is based on task names and tracking systems defined above
-    for sub_id in matched_control_sub_ids:
+    # file name is based on task names and tracking systems defined above
+    for sub_id in pd_sub_ids + matched_control_sub_ids:
         for task_name in TASK_NAMES:
-            for tracksys in TRACKING_SYSTEMS:    
-                file_name = f"sub-{sub_id}_task-{task_name}_tracksys-{tracksys}_motion.tsv"
-                file_path = f"{RAW_DATA_PATH}\\sub-{sub_id}\\motion\\{file_name}"
+            for tracksys in TRACKING_SYSTEMS:
+                for run in RUN:
 
-                # Load the data as a pandas dataframe
-                data = data_loader.load_file(file_path=file_path)
+                    # change current working directory to the folder with motion data
+                    os.chdir(f'{RAW_DATA_PATH}\\sub-{sub_id}\\motion')
+                    file_list = os.listdir()
 
-                # Fill the gaps
-                interpolated_data = interpolate.fill_gaps(data)
+                    file_path = None  # Initialize file_path for each loop
 
-                # Filtering
-                preprocessed_data = filter.butter_lowpass_filter(data=interpolated_data, fs=200., cutoff=5.0)
+                    for file in file_list:
+                        if sub_id in file and task_name in file and tracksys in file:
+                            # Check if the 'run' condition matches 
+                            if any(f"run-{r}" in file for r in RUN):
+                                file_path = f"{RAW_DATA_PATH}\\sub-{sub_id}\\motion\\{file}"
+                                break # Exit the loop once a matching file is found
 
-                # Principal component analysis (to align )
+                            # Include files without any 'run-on' or 'run-off' condition (run-on and run-off are only applicable to PD)
+                            elif not any(f"run-{cond}" in file for cond in ["on", "off"]):
+                                file_path = f"{RAW_DATA_PATH}\\sub-{sub_id}\\motion\\{file}"
+                                break
+                            
+                            
+                        #     and run in file:
+                        #     file_path = f"{RAW_DATA_PATH}\\sub-{sub_id}\\motion\\{file}"
+                        # elif sub_id in file and task_name in file and tracksys in file and 'run-off' not in file:
+                        #     file_path = f"{RAW_DATA_PATH}\\sub-{sub_id}\\motion\\{file}"
 
-                rotated_data = align.pca(data=preprocessed_data)
-        
-                # Calculate kinectome
-                kinectome = src.kinectome.calculate_kinectome(data=rotated_data)
-        
-                # Modularity analysis
+                if file_path:
+                    # Load the data as a pandas dataframe
+                    data = data_loader.load_file(file_path=file_path)
 
+                    # Fill the gaps
+                    interpolated_data = interpolate.fill_gaps(data)
+
+                    # Filtering
+                    preprocessed_data = filter.butter_lowpass_filter(data=interpolated_data, fs=200., cutoff=5.0)
+
+                    # Principal component analysis (to align )
+
+                    rotated_data = align.pca(data=preprocessed_data)
+            
+                    # Calculate kinectome
+                    kinectome = src.kinectome.calculate_kinectome(data=rotated_data)
+            
+                    # Modularity analysis
+
+                else:
+                    print(f"No matching file found for sub-{sub_id}, task-{task_name}, tracksys-{tracksys}, run-{run}")
+                    
     return
 
 
