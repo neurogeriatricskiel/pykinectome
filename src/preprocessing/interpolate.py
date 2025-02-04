2da5b865e6e6f1a19073db144c296f1f6184529e
Karolina <karolina.saegner@uksh.de>
2025-02-04 15:34:40 +0100

    fill the gaps using kinetics toolkit. print how many missing datapoints are left (when the value is above the threshold)

--- src/preprocessing/interpolate.py
+++ src/preprocessing/interpolate.py
@@ -5,7 +5,7 @@ import matplotlib
 matplotlib.use('Agg')  # Use a non-interactive backend
 import matplotlib.pyplot as plt
 
-def fill_gaps(data, task_name, fc, threshold):
+def fill_gaps(data: pd.DataFrame, sub_id: str, task_name: str, fc: float, threshold: float) -> pd.DataFrame:
     """
     Identify and handle consecutive NaNs in the DataFrame.
     
@@ -44,6 +44,24 @@ def fill_gaps(data, task_name, fc, threshold):
     # make an if statement(s) that if any marker in the cluster, or e.g., radial side of the wrist is missing (while ulnar is visible), then the threshold should not exist
     # or is this made automatically?  
     filled_markers = ts.fill_missing_samples(max_missing_samples=threshold) 
+
+    filled_df = pd.DataFrame(filled_markers.data)
+
+    for col in filled_df:
+        max_nan_streak = 0  # Variable to store the longest NaN sequence
+        is_nan = filled_df[col].isna().astype(int)
+        nan_streaks = is_nan.groupby((is_nan != is_nan.shift()).cumsum()).cumsum()
+        max_streak = nan_streaks.max()  # Get the longest streak in this column
+
+        if max_streak > max_nan_streak:
+            max_nan_streak = max_streak  # Update the global max
+
+            if max_nan_streak != 0:
+                print('\n', task_name)
+                print(max_nan_streak, col)
+
+    if pd.DataFrame(filled_markers.data).isnull().values.any():
+        print(f'{sub_id} {task_name} has NaNs')
 
     # filter
     filled_markers = ktk.filters.butter(filled_markers, fc=fc) # default order = 2
