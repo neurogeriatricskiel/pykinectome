8931c198d7b88fc646e3cca32b55c1b7f15c7972
Karolina <karolina.saegner@uksh.de>
2025-02-04 15:39:09 +0100

    updated .gitignore so no test plots go into the repository

--- src/preprocessing/align.py
+++ src/preprocessing/align.py
@@ -1,4 +1,16 @@
+from sklearn.decomposition import PCA
+import pandas as pd
+
 # Use PCA for aligning marker data with main walking direction
 
-def pca(data):
-    return data
+def pca(data: pd.DataFrame):
+    ''' Runs principal component analysis.
+    Returns a rotation matrix'''
+    # apply principal component analysis
+    pca = PCA(n_components=2)
+    pca.fit(X=data)
+
+    # get the rotation matrix
+    rotation_matrix = pca.components_
+
+    return rotation_matrix
