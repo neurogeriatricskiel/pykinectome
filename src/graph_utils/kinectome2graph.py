59490a3e7a687dd6b75ed3f720fe0b2dac2b57ad
Karolina <karolina.saegner@uksh.de>
2025-04-03 14:59:19 +0200

    build_graph for full graph works by simply adding a dummy dimension (then the matrix dimensions match the n x n x 3 kinectome)

--- src/graph_utils/kinectome2graph.py
+++ src/graph_utils/kinectome2graph.py
@@ -2,32 +2,26 @@ import numpy as np
 import networkx as nx
 
 
-def build_graph(kinectome, marker_list, bound_value=None,flag_shift = False, full_graph = False):
-    """Builds weighted graphs for AP, ML, V directions while preserving meaningful negative correlations.
-    Parameters:
-    ----------
-    kinectome : np.ndarray
-        A 3D numpy array containing the kinectome data.
-    marker_list : list
-        A list of marker names.
-    bound_value : float, optional
-        The threshold value for edge weights. If None, all edges are included.
-    flag_shift : bool, optional
-        Flag to indicate whether to shift weights to be non-negative.
-    full_graph : bool, optional
-        Flag to indicate whether to build a full graph.I.e, A graph with all the directions combined.
-    Returns:    
-    -------
-    graphs : list
-        A list of weighted graphs for each direction (AP, ML, V), if full_graph is False.
-    G : nx.Graph
-        A weighted graph with all directions combined, if full_graph is True.
+def build_graph(kinectome, marker_list, bound_value=None):
 
-    """
-    if full_graph:
+    """Builds weighted graphs for AP, ML, V directions if ndim==2, 
+    else builds one graph for the full kinectome (containins all directions)     
+    while preserving meaningful negative correlations."""
+    
+    # np.expand_dims
+    # kinectome = kinectome[..., None] if kinectome.ndim == 2 else kinectome
+    directions = ['AP', 'ML', 'V']
+    marker_list = (
+                    [f"{m}_{d}" for m in marker_list for d in directions] if kinectome.ndim == 2 else marker_list
+                    )
+    kinectome = np.expand_dims(kinectome, axis=-1) if kinectome.ndim == 2 else kinectome
+     
+    
+    graphs = []
+    for direction in range(kinectome.shape[-1]):
         G = nx.Graph()
         num_nodes = kinectome.shape[0]
-        min_weight = np.min(kinectome)
+        min_weight = np.min(kinectome[:, :, direction])
         shift = -min_weight if min_weight < 0 else 0  # Shift weights to be non-negative
         
         # Add nodes with marker labels
@@ -36,10 +30,7 @@ def build_graph(kinectome, marker_list, bound_value=None,flag_shift = False, ful
 
         for i in range(num_nodes):
             for j in range(i + 1, num_nodes):
-                if flag_shift:
-                    weight = kinectome[i, j] + shift  # Apply shift
-                else:
-                    weight = kinectome[i, j]
+                weight = kinectome[i, j, direction] + shift  # Apply shift
                 if bound_value is None:
                     if not np.isnan(weight):
                         G.add_edge(marker_list[i], marker_list[j], weight=weight)
@@ -47,35 +38,10 @@ def build_graph(kinectome, marker_list, bound_value=None,flag_shift = False, ful
                     if not np.isnan(weight) and weight >= bound_value:
                         G.add_edge(marker_list[i], marker_list[j], weight=weight)
         
-        return G
-    else:
-        graphs = []
-        for direction in range(kinectome.shape[2]):
-            G = nx.Graph()
-            num_nodes = kinectome.shape[0]
-            min_weight = np.min(kinectome[:, :, direction])
-            shift = -min_weight if min_weight < 0 else 0  # Shift weights to be non-negative
-            
-            # Add nodes with marker labels
-            for i in range(num_nodes):
-                G.add_node(marker_list[i])  # Assign marker name as node label
+        graphs.append(G)
+    
+    return graphs
 
-            for i in range(num_nodes):
-                for j in range(i + 1, num_nodes):
-                    if flag_shift:
-                        weight = kinectome[i, j, direction] + shift  # Apply shift
-                    else:
-                        weight = kinectome[i, j, direction]
-                    if bound_value is None:
-                        if not np.isnan(weight):
-                            G.add_edge(marker_list[i], marker_list[j], weight=weight)
-                    else: # Apply threshold
-                        if not np.isnan(weight) and weight >= bound_value:
-                            G.add_edge(marker_list[i], marker_list[j], weight=weight)
-            
-            graphs.append(G)
-        
-        return graphs
 
 
 def weighted_degree_centrality(G):
