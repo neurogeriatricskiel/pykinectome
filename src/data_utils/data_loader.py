36731c40b0443e8c5c1cc2b924b67308ea2bdfa8
Karolina <karolina.saegner@uksh.de>
2025-07-28 13:57:23 +0200

    load interpolated kinectomes

--- src/data_utils/data_loader.py
+++ src/data_utils/data_loader.py
@@ -61,23 +61,35 @@ def load_events(base_path, sub_id, task_name, run,linux=False):
 
     return events
 
-def load_kinectomes(base_path, sub_id, task_name, tracksys, run, kinematics, full, correlation_method):
+def load_kinectomes(base_path, sub_id, task_name, tracksys, run, kinematics, full, correlation_method, interpol=False):
     """Loads kinectome files and sorts them by onset indices."""
     try:
         os.chdir(f'{base_path}/derived_data/sub-{sub_id}/kinectomes')
         file_list = os.listdir()
         
-        # run 'on' or 'off' only exists in the file names of pwPD 
+        # run 'on' or 'off' only exists in the file names of pwPD
         if run:            
             if full:
-                relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, run, kinematics, correlation_method, 'full'])]
+                if interpol:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, run, kinematics, correlation_method, 'full', 'interpol'])]
+                else:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, run, kinematics, correlation_method, 'full']) and 'interpol' not in file]
             else:
-                relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, run, kinematics, correlation_method]) and 'full' not in file]
+                if interpol:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, run, kinematics, correlation_method, 'interpol']) and 'full' not in file]
+                else:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, run, kinematics, correlation_method]) and 'full' not in file and 'interpol' not in file]
         else:            
             if full:
-                relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, kinematics, correlation_method, 'full'])]
+                if interpol:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, kinematics, correlation_method, 'full', 'interpol'])]
+                else:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, kinematics, correlation_method, 'full']) and 'interpol' not in file]
             else:
-                relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, kinematics, correlation_method]) and 'full' not in file]
+                if interpol:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, kinematics, correlation_method, 'interpol']) and 'full' not in file]
+                else:
+                    relevant_files = [file for file in file_list if all(x in file for x in [task_name, tracksys, kinematics, correlation_method]) and 'full' not in file and 'interpol' not in file]
         
         sorted_files = sorted(relevant_files, key=lambda file: extract_onset_indices(file)[0])
         
